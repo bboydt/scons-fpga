42b29fa2acfe6c6ae2fd61569a9d0074692cb520
Boyd Thomas <boydbt48@gmail.com>
2024-01-31 23:30:31 -0500

    Added VVPRun for running icarus verilog simulations

--- icarus.py
+++ icarus.py
@@ -2,39 +2,49 @@
 #
 # Builders
 # - VVPFile: builds a vvp file that can be run with vvp
+#
+# Methods:
+# - VVPRun: adds an action that run for a given vvp file
 
 from SCons.Builder import Builder
 from SCons.Action import Action
 
-vvp_builder = None
-vvp_action = None
+vvp_file_builder = None
+vvp_file_action = None
+vvp_run_action = None
+
+def vvp_run_method(self, target):
+    self.AddPostAction(target, vvp_run_action)
 
 def generate(env):
 
     # Create actions and builders
     #
 
-    global vvp_builder
-    if (vvp_builder is None):
-        vvp_builder = Builder(action = {},
+    global vvp_file_builder
+    if (vvp_file_builder is None):
+        vvp_file_builder = Builder(action = {},
                               prefix = "$VVPPREFIX",
                               suffix = "$VVPSUFFIX",
                               emitter = {},
                               source_ext_match = None,
                               single_source = False)
 
-    global vvp_action
-    if (vvp_action is None):
-        vvp_action = Action("$VVPCOM", "$VVPCOMSTR")
+    global vvp_file_action
+    if (vvp_file_action is None):
+        vvp_file_action = Action("$VVPCOM", "$VVPCOMSTR")
+
+    global vvp_run_action
+    if (vvp_run_action is None):
+        vvp_run_action = Action("$VVPRUNCOM", "$VVPRUNCOMSTR")
 
     # Prep environment
     #
 
     if ("VERILOGPREFIX" not in env):
-        env["VERILOGPREFIX"] = ""
-
+        env["VERILOGPREFIX"] = "";
     if ("VERILOGSUFFIX" not in env):
-        env["VERILOGSUFFIX"] = ".v"
+        env["VERILOGSUFFIX"] = "";
 
     env["VPPPREFIX"] = ""
     env["VPPSUFFIX"] = ".vvp"
@@ -56,12 +66,18 @@ def generate(env):
 
     env["VVPCOM"] = "$IV $IVFLAGS $_VINCFLAGS $_VDEFFLAGS $_VPRMFLAGS -o $TARGET $SOURCES"
 
+    env["VVP"] = "vvp"
+    env["VVPFLAS"] = ""
+    env["VVPRUNCOM"] = "$VVP $VVPFLAGS $TARGET"
+
     # Add builders
     #
 
-    vvp_builder.add_action(".v", vvp_action)
+    vvp_file_builder.add_action(".v", vvp_file_action)
 
-    env["BUILDERS"]["VVPFile"] = vvp_builder
+    env["BUILDERS"]["VVPFile"] = vvp_file_builder
+
+    env.AddMethod(vvp_run_method, "VVPRun")
 
 def exists():
     return True
